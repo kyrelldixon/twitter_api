d5906fd5692580a9d8fc566555bf6edd5df74146
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-24 09:43:05 -0500

    Added cert and https for dev

--- config/dev.exs
+++ config/dev.exs
@@ -8,6 +8,12 @@ use Mix.Config
 # with webpack to recompile .js and .css sources.
 config :twitter_api, TwitterApiWeb.Endpoint,
   http: [port: 4000],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    certfile: "priv/cert/selfsigned.pem",
+    keyfile: "priv/cert/selfsigned_key.pem"
+  ],
   debug_errors: false,
   code_reloader: true,
   check_origin: false,
