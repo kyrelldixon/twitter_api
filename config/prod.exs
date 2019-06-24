23098452f859e25b29a8653900b3395f726b1e47
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-24 05:27:10 -0500

    Updated config

--- config/prod.exs
+++ config/prod.exs
@@ -10,10 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :twitter_api, TwitterApiWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  # http: [:inet6, port: System.get_env("PORT") || 4000],
+  load_from_system_env: true,
   url: [scheme: "https", host: System.get_env("HOST"), port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
