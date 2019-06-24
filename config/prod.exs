2fdb9684b678ec2603fc06a4a5d0919cb0e25e7c
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-24 05:18:15 -0500

    Added procfile and database configuration

--- config/prod.exs
+++ config/prod.exs
@@ -19,6 +19,13 @@ config :twitter_api, TwitterApiWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Configure your database
+config :twitter_api, TwitterApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true,
+  url: System.get_env("DATABASE_URL")
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
