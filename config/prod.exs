00a2ebfc6fc7dcc10498f0cec454ddfb8e17f252
Kyrell Dixon <kyrell.brown@gmail.com>
2019-06-24 09:43:16 -0500

    removed cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,6 @@ config :twitter_api, TwitterApiWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: System.get_env("HOST"), port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto], hsts: true],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
